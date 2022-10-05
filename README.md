20a9c4be532f8fd0754880288781effe913488d1
Adam W <48334586+adamargyle@users.noreply.github.com>
2022-10-05 10:25:02 -0400

    removed broken testing option

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Organizing files and folders for Plex based on existing metadata within the file.
  
  Usage:
-`plexorganizer.sh` `source` `destination` `testing(optional)`
+`plexorganizer.sh` `source` `destination`
  Run the script from a source folder of encoded videos with metadata including at least title and release data and it will create folders with a movie name including the year and movie file within.
 
  Dependencies: 
--- plexorganizer.sh
+++ plexorganizer.sh
@@ -2,32 +2,15 @@
 
 readonly source=$1
 readonly destination=$2
-#readonly testing=$3
 
 echo "source is "$source
 echo "destination is "$destination
 
-# somehow everything seemed to turn on testing mode... 
-#if [ $testing =  ]
-# echo "testing mode is on"
-# then
-#     for file in $source/*
-#     do
-#         local title=$(ffmpeg -y -loglevel error -i ${file} -f ffmetadata - | grep -i 'title' | cut -d '=' -f2 | grep -v -e "Chapter" | grep -v -e "<")
-#         local year=$(ffmpeg -y -loglevel error -i ${file} -f ffmetadata - | grep -i 'date' | grep -v -e "<" | cut -d '=' -f2 | cut -d '-' -f1 )
-#         local full_title="${title} (${year})"
-#         echo "${full_title}"
-#     done
-#     echo "testing mode complete"
-#     exit 0
-# fi
-
 for file in $source/*
 do
-    # further logic needed: check if item is a movie or tv show, process tv show with separate workflow
-    # take all metadata in one variable, check media type (9=movie, 10=tv), then pull out release year and title on movies, and tv show, episode title, season and episode number on tv shows. check first if a folder exists
-    #remove unrated edition/director's cut/ etc or use the new edition tags for plex instead
-    #check for file characters that might not be good (punctuation mostly)
+
+    # remove unrated edition/director's cut/ etc or use the new edition tags for plex instead
+    # check for file characters that might not be good (punctuation mostly)
 
     local metadata=$(ffmpeg -y -loglevel error -i ${file} -f ffmetadata - )
     local media_type=$(grep -i 'media_type' <<<$metadata | cut -d '=' -f2)
